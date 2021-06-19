e4600bc83cc867f332cda84b2c0226ba4a9d00d1
zhangyu836 <zhangyu836@gmail.com>
2021-06-19 18:33:41 +0800

    2

--- loadjs.js
+++ loadjs.js
@@ -1,1 +1,1 @@
-const j="//cdn.jsdelivr.net/gh/zhangyu836/www/",d="./";let h=window.location.hostname,p=h.includes("jsd")?j:d,b=p+"bundle.js";$.getScript(b);let pf=p+"pdf.js";$.getScript(pf);
+const j="//cdn.jsdelivr.net/gh/zhangyu836/www/",d="./";let h=window.location.hostname,p=h.includes("xyz")?j:d,b=p+"bundle.js";$.getScript(b);let pf=p+"pdf.js";$.getScript(pf);
